556346f6f4cc487183172c24d4b1bb8bf2a64c64
Paul Adams <paul.adams@scout24.com>
2021-01-23 21:44:13 +0100

    Fix OG image

--- components/Page.js
+++ components/Page.js
@@ -19,7 +19,7 @@ export default function BlogPostLayout({ meta, children }) {
                 <meta name="twitter:creator" content={meta.author.twitter} key="twhandle" />
 
                 <meta property="og:url" content={router.pathname} key="ogurl" />
-                <meta property="og:image" content={meta.image} key="ogimage" />
+                <meta property="og:image" content={`${router.pathname}/assets/cover/${meta.image}`} key="ogimage" />
                 <meta property="og:site_name" content="Baggerspion" key="ogsitename" />
                 <meta property="og:title" content={meta.title} key="ogtitle" />
                 <meta property="og:description" content={meta.description} key="ogdesc" />
