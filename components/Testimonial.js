40a42ee35f712119d5c0c6bb0a9b35b5602070b7
Paul Adams <paul.adams@scout24.com>
2021-01-24 14:51:15 +0100

    Better testimonial design

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -4,8 +4,14 @@ import Image from 'next/image'
 
 export default function Testimonial({ data }) {
     return (
-        <div className="flex flex-col">
-            <div className="flex flex-col mb-4">
+        <div className="flex flex-col pt-8">
+            <div>
+                <span className="text-yellow-500 text-2xl inline mr-4">
+                    <FontAwesomeIcon icon={faQuoteLeft} />
+                </span>
+                <p className="prose inline">{data.content}</p>
+            </div>
+            <div className="flex flex-col pt-6">
                 <Image
                     className="block rounded-full"
                     src={`/assets/testimonials/${data.image}`}
@@ -15,12 +21,6 @@ export default function Testimonial({ data }) {
                 />
                 <span className="font-bold text-sm">{data.name}</span>
                 <span className="text-sm">{data.affilliation}</span>
-            </div>
-            <div>
-                <span className="text-yellow-500 text-2xl inline mr-4">
-                    <FontAwesomeIcon icon={faQuoteLeft} />
-                </span>
-                <p className="prose inline">{data.content}</p>
             </div>
         </div>
     )
--- components/Testimonials.js
+++ components/Testimonials.js
@@ -2,7 +2,7 @@ import Testimonial from 'components/Testimonial'
 
 export default function Testimonials({ data }) {
     return (
-        <div className="grid grid-cols-1 gap-8">
+        <div className="grid grid-cols-1 gap-8 divide-y divide-gray-300">
             {data.map(testimonial =>
                 <Testimonial data={testimonial} />
             )}
