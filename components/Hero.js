563b41123cf8749c5d7255de24a86d143181dc3e
Paul Adams <paul.adams@scout24.com>
2021-01-30 12:55:48 +0100

    Better coloring of hero

--- components/Hero.js
+++ components/Hero.js
@@ -41,13 +41,13 @@ export default function Hero({ posts }) {
                     </div>
                 </div>
             )}
-            <div className="absolute flex h-full items-center left-6 md:left-12 z-10">
-                <button onClick={prevHero} type="button" className="block focus:outline-none text-yellow-500 hover:text-black hover:opacity-50 text-2xl">                        
+            <div className="absolute flex h-full items-center left-6 md:left-12 z-10 rounded-full">
+                <button onClick={prevHero} type="button" className="block focus:outline-none text-yellow-500 opacity-50 hover:opacity-100 text-2xl">                        
                     <FontAwesomeIcon icon={faChevronCircleLeft} />
                 </button> 
             </div> 
             <div className="absolute flex h-full items-center right-6 md:right-12 z-10">
-                <button onClick={nextHero} type="button" className="block focus:outline-none text-yellow-500 hover:text-black hover:opacity-50 text-2xl">
+                <button onClick={nextHero} type="button" className="block focus:outline-none text-yellow-500 opacity-50 hover:opacity-100 text-2xl">
                     <FontAwesomeIcon icon={faChevronCircleRight} />
                 </button>
             </div>
