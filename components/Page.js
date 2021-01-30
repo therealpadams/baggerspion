d1d223a47c5a4c7c5b46a298b008b948e0e13710
Paul Adams <paul.adams@scout24.com>
2021-01-30 13:43:11 +0100

    Improved OG usage

--- components/Page.js
+++ components/Page.js
@@ -15,14 +15,15 @@ export default function Page({ meta, children }) {
     return (
         <>
             <Head>
-                <meta name="twitter:card" content="summary" key="twcard" />
-                <meta name="twitter:creator" content={meta.author.twitter} key="twhandle" />
+                <meta name="twitter:card"       content="summary" key="twcard" />
+                <meta name="twitter:creator"    content={meta.author.twitter} key="twhandle" />
 
-                <meta property="og:url" content={`https://baggerspion.therealpadams.vercel.app${router.pathname}`} key="ogurl" />
-                <meta property="og:image" content={`https://baggerspion.therealpadams.vercel.app/assets/cover/${meta.image}`} key="ogimage" />
-                <meta property="og:site_name" content="Baggerspion" key="ogsitename" />
-                <meta property="og:title" content={meta.title} key="ogtitle" />
+                <meta property="og:url"         content={`https://baggerspion.therealpadams.vercel.app${router.pathname}`} key="ogurl" />
+                <meta property="og:type"        content="article" />
+                <meta property="og:title"       content={meta.title} key="ogtitle" />
                 <meta property="og:description" content={meta.description} key="ogdesc" />
+                <meta property="og:image"       content={`https://baggerspion.therealpadams.vercel.app/assets/cover/${meta.image}`} key="ogimage" />
+                <meta property="og:site_name"   content="Baggerspion" key="ogsitename" />
 
                 <title>{`Baggerspion: ${meta.title}`}</title>
             </Head>
