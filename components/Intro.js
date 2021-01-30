3261d057a161acebc34e232b2c5fd3ce950e0ca2
Paul Adams <paul.adams@scout24.com>
2021-01-30 13:00:33 +0100

    Better intro section

--- components/Intro.js
+++ components/Intro.js
@@ -1,15 +1,19 @@
+import Container from 'components/Container'
+
 export default function Intro() {
     return (
-        <section className="flex flex-col px-6 md:px-12 pt-12 pb-6 md:pb-12">
-            <h2 className="text-2xl">Welcome to Baggerspion.</h2>
-            <h3 className="leading-9 font-extrabold tracking-tight text-gray-900 text-4xl">Engineering Leadership &amp; Culture.</h3>
-            <p className="prose mt-4">
-            You've stumbled upon the blog of Paul Adams. Sorry about that. 
-            I'm a broadly-experienced (read: "expert in nothing") engineering 
-            leader based in Berlin. When I make the effort to write, I mostly 
-            write about engineering topics I care about. Very ocasionally, 
-            you might catch me writing about my life in Germany.
-            </p>
+        <section className="flex flex-col pt-12 pb-6">
+            <Container>
+                <h2>Welcome to Baggerspion.</h2>
+                <span className="text-4xl font-bold">Engineering Leadership &amp; Culture.</span>
+                <p className="mt-4">
+                You've stumbled upon the blog of Paul Adams. Sorry about that. 
+                I'm a broadly-experienced (read: "expert in nothing") engineering 
+                leader based in Berlin. When I make the effort to write, I mostly 
+                write about engineering topics I care about. Very ocasionally, 
+                you might catch me writing about my life in Germany.
+                </p>
+            </Container>
         </section>
     )
 }
