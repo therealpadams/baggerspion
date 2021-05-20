27c59c714a8cc43fca7c9e9529937cff620b8a5e
Paul Adams <paul@vercel.com>
2021-05-20 17:54:34 +0200

    Better spacing in header

--- components/layouts/blog/Header.js
+++ components/layouts/blog/Header.js
@@ -4,10 +4,10 @@ import Image from 'next/image'
 export default function Header({ meta }) {
     return (
         <>
-            <h1 className="block ">{meta.title}</h1>
+            <h1 className="block">{meta.title}</h1>
             {meta.author.name ? <AuthorDetail author={meta.author} date={meta.date} /> : ""}
             <hr />
-            {meta.description ? <p className="italic pt-4">{meta.description}</p> : <></>}
+            {meta.description ? <p className="italic pt-6">{meta.description}</p> : <></>}
             <div className="relative w-full h-96 my-8">
                 <Image
                     src={`/assets/cover/${meta.image}`}
