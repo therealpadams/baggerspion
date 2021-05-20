3be9a55ee0a18bed94682ada8b0f4919fe105311
Paul Adams <paul@vercel.com>
2021-05-20 19:40:14 +0200

    Better layout of the company locals

--- components/sections/about/Companies.js
+++ components/sections/about/Companies.js
@@ -4,15 +4,15 @@ export default function Companies({ data }) {
     return (
         <>
             <h2>Organisations With Which I've Worked</h2>
-            <div className="grid grid-cols-5 gap-4">
+            <div className="grid grid-cols-5 gap-2">
                 {data.map(company => (
                     <Image
                         key={company.name}
                         className="grayscale"
-                        width={70}
-                        height={70}
-                        layout="fixed"
+                        width={20}
+                        height={20}
                         src={`/assets/work/${company.logo}`}
+                        layout="responsive"
                     />
                 ))}
             </div>
