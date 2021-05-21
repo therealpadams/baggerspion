455243e42b90bb6f4fd9c8c03571eed43f08c189
Paul Adams <paul@vercel.com>
2021-05-21 10:07:29 +0200

    New, shorter intro

--- components/sections/home/Intro.js
+++ components/sections/home/Intro.js
@@ -4,11 +4,9 @@ export default function Intro() {
             <h2 className="text-2xl font-bold">Welcome to Baggerspion.</h2>
             <span className="text-4xl font-bold">Engineering Leadership &amp; Culture.</span>
             <p className="mt-4">
-            You've stumbled upon the blog of Paul Adams. Sorry about that. 
-            I'm a broadly-experienced (read: "expert in nothing") engineering 
-            leader based in Berlin. When I make the effort to write, I mostly 
-            write about engineering topics I care about. Very ocasionally, 
-            you might catch me writing about my life in Germany.
+            Baggerspion is the personal website of Paul Adams, an engineering leader based in Berlin.
+            Here you will find his thoughts on leadership and culture in software engineering, covering
+            topics such as agility and open source... as well as the occasional post about working/living in Berlin!
             </p>
         </section>
     )
