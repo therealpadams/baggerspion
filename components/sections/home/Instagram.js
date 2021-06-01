36ace69af024864722fd4417d1e0d30ee47db24c
Paul Adams <paul@baggerspion.net>
2021-06-01 10:42:12 +0200

    Completed Insta feed

--- components/sections/home/Instagram.js
+++
@@ -1,7 +0,0 @@
-export default function Instagram() {
-    return (
-        <section className="mx-auto prose px-6">
-            <h2 className="text-2xl font-bold">Gallery</h2>
-        </section>
-    )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Archive from 'components/sections/home/Archive'
 import Head from 'next/head'
 import Hero from 'components/sections/home/Hero'
-import Instagram from 'components/sections/home/Instagram'
+import Instagram from "instagram-web-api"
+import Gallery from 'components/sections/home/Gallery'
 import Intro from 'components/sections/home/Intro'
 import Layout from 'components/layouts/primary/Primary'
 import { posts } from 'lib/getAllPosts'
 
-export default function Home() {
+export default function Home({ instagramPosts }) {
     const meta = {
         title: "Home"
     }
@@ -20,8 +21,36 @@ export default function Home() {
                 <Hero posts={posts.slice(0, 3)} />
                 <Intro />
                 <Archive posts={posts.slice(3, 9)} />
-                <Instagram />
+                <Gallery posts={instagramPosts} />
             </Layout>
         </>
     )
+}
+
+export async function getServerSideProps() {
+    const client = new Instagram({
+        username: process.env.IG_USERNAME,
+        password: process.env.IG_PASSWORD,
+    })
+    
+    let posts = []
+    try {
+        await client.login()
+        
+        const instagram = await client.getPhotosByUsername({
+            username: process.env.IG_USERNAME,
+        })
+    
+        if (instagram["user"]["edge_owner_to_timeline_media"]["count"] > 0) {
+            posts = instagram["user"]["edge_owner_to_timeline_media"]["edges"]
+        }
+      } catch (err) {
+        console.log("Something went wrong while fetching content from Instagram", err)
+      }
+    
+      return {
+        props: {
+            instagramPosts: posts,
+        },
+    }
 }
