4c504c52cd7357fb2c4dfb368db10e7aaf4650f0
Paul Adams <paul@baggerspion.net>
2021-06-01 08:56:51 +0200

    Added empty section for Gallery on homepage

---
+++ components/sections/home/Instagram.js
@@ -0,0 +1,7 @@
+export default function Instagram() {
+    return (
+        <section className="mx-auto prose px-6">
+            <h2 className="text-2xl font-bold">Gallery</h2>
+        </section>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Archive from 'components/sections/home/Archive'
 import Head from 'next/head'
 import Hero from 'components/sections/home/Hero'
+import Instagram from 'components/sections/home/Instagram'
 import Intro from 'components/sections/home/Intro'
 import Layout from 'components/layouts/primary/Primary'
 import { posts } from 'lib/getAllPosts'
@@ -19,6 +20,7 @@ export default function Home() {
                 <Hero posts={posts.slice(0, 3)} />
                 <Intro />
                 <Archive posts={posts.slice(3, 9)} />
+                <Instagram />
             </Layout>
         </>
     )
